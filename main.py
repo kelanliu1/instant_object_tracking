06663ec872ad00644fcae4aafab8bf77a6cfb21c
kelan liu <kel323@lehigh.edu>
2023-04-07 01:19:39 -0400

    vid process & tests

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-import json
 import os
 import cv2
 import torch
-import yaml
-import numpy as np
 import torch
-from sort import Sort
+from sort import *
 from flask import Flask, request, jsonify
 import uuid
 import threading
-import time
 import queue
 
-
-job_queue = queue.Queue()
-
+# Initialize the Flask app
 app = Flask(__name__)
 
-video_jobs, job_status = {}, {}
+### Global variables ##################################################################################################
+DEBUG = False
+video_jobs = {}
 video_processing_queue = queue.Queue()
 
 # Load the YOLOv5 model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-
-def load_classes(file):
-    with open(file, 'r') as f:
-        config = yaml.safe_load(f)
-    return config['names']
-
-classes = load_classes('coco.yaml')
+#######################################################################################################################
 
 def worker():
     while True:
@@ -36,12 +26,11 @@ def worker():
         process_video(job_id, source_url)
         video_processing_queue.task_done()
 
-num_worker_threads = 4  # Adjust this based on your system's capabilities
+num_worker_threads = 4  # Change this to adjust the number of parallel workers
 for _ in range(num_worker_threads):
     t = threading.Thread(target=worker)
     t.daemon = True  # Mark as a daemon thread so it exits when the main thread exits
     t.start()
-
 
 # Route to handle push
 @app.route('/push', methods=['POST'])
@@ -58,55 +47,53 @@ def push():
     # Add the job to the processing queue
     video_processing_queue.put((job_id, source_url))
 
-    # Return the job ID to the client
-    return jsonify({'id': job_id})
+    # Update the job status
+    video_jobs[job_id] = {
+        'status': 'queued',
+        'data': None,
+    }
 
+    # Return the job ID to the client
+    return jsonify({'id (/push)': job_id})
 
 # Route to handle status
-@app.route('/status/<job_id>', methods=['GET']) 
-def status(job_id):
-    if job_id in video_jobs:
-        return jsonify({'status': video_jobs[job_id]})
-    else:
-        return jsonify({'error': 'job id not found'}), 404
-
-# Route to handle query
-@app.route('/query/<id>', methods=['GET'])
-def query(id):
+@app.route('/status/<id>', methods=['GET']) 
+def status(id):
     if id in video_jobs:
         if video_jobs[id]['status'] == 'finished':
-            return jsonify({'data': video_jobs[id]['data']})
-        else:
-            return jsonify({'error': 'Video is still processing'}), 400
+            return jsonify({'status': 'finished'})
+        elif video_jobs[id]['status'] == 'processing':
+            return jsonify({'status': 'processing'})
+        elif video_jobs[id]['status'] == 'queued':
+            return jsonify({'status': 'queued'})
     else:
-        return jsonify({'error': 'ID not found'}), 404
+        return jsonify({'error (/status)': 'ID not found'}), 404
+
+# Route to handle query
+@app.route('/query/<job_id>', methods=['GET'])
+def query(job_id):
+    if job_id in video_jobs:
+        return jsonify({'data (/query)': video_jobs[job_id]['data']})
+    else:
+        return jsonify({'error (/query)': 'job id not found'}), 404
 
 # Route to handle list
 @app.route('/list', methods=['GET'])
 def list_jobs():
-    return jsonify({'data': list(video_jobs.keys())})
+    return jsonify({'data (/list)': list(video_jobs.keys())})
 
-def get_tracked_objects(detections):
-    tracked_objects = []
-    for frame_det in detections:
-        if frame_det.size == 0:  # Check if frame_det is empty
-            continue  # Skip this iteration if frame_det is empty
-
-        frame_number = frame_det[0]
-        for det in frame_det[1:]:
-            x1, y1, x2, y2, obj_id, cls_id, conf = det
-            tracked_objects.append((int(obj_id), int(frame_number), int(cls_id), float(conf), int(x1), int(y1), int(x2), int(y2)))
-    return tracked_objects
-
-# process video
 def process_video(job_id, source_url, yolo_model_name='yolov5s'):
-    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    video_jobs[job_id] = {
+        'status': 'processing',
+        'data': None,
+    }
 
     cap = cv2.VideoCapture(source_url)
     frame_number = 0
     detections = []
 
-    # Initialize the SORT tracker
+    model.float()
+    model.eval()
     tracker = Sort()
 
     while cap.isOpened():
@@ -114,41 +101,38 @@ def process_video(job_id, source_url, yolo_model_name='yolov5s'):
         if not ret:
             break
 
-        # Resize the frame to the input size expected by the YOLOv5 model
-        input_size = 640  # This is the default input size for the YOLOv5 model; adjust if necessary
-        frame = cv2.resize(frame, (input_size, input_size))
+        preds = model(frame)
+        yolov5_detections = preds.pred[0].numpy()
+        tracked_objects = tracker.update(yolov5_detections)
 
-        # YOLOv5 detection
-        results = model(frame)
+        for j in range(len(tracked_objects.tolist())):
+            coords = tracked_objects.tolist()[j]
+            x1, y1, x2, y2, track_id = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3]), int(coords[4])
 
-        # Convert the detections to the format required by the SORT tracker
-        det_for_sort = []
-        for *xyxy, conf, cls in results.xyxy[0]:
-            x1, y1, x2, y2 = xyxy
-            det_for_sort.append([x1.item(), y1.item(), x2.item(), y2.item(), conf.item()])
+            class_idx = int(yolov5_detections[j, 5])
+            class_name = preds.names[class_idx]
+            class_prob = float(yolov5_detections[j, 4])
 
-        det_for_sort = np.array(det_for_sort)
-
-        # Update the SORT tracker with the detections if there are any
-        if len(det_for_sort) > 0:
-            tracked_objects = tracker.update(det_for_sort)
-        else:
-            tracked_objects = np.array([])
-
-        # Add the frame number to the tracked objects and append them to the detections list
-        if tracked_objects.ndim > 1:
-            detections.append(np.insert(tracked_objects, 0, frame_number, axis=1))
+            detections.append({
+                'frame': frame_number,
+                'x1': x1,
+                'y1': y1,
+                'x2': x2,
+                'y2': y2,
+                'id': int(track_id),
+                'class_id': int(class_idx),
+                'class_name': class_name,
+                'confidence': class_prob,
+            })
 
         frame_number += 1
 
-    cap.release()
-
-    # Save results and update job status
-    tracked_objects = get_tracked_objects(detections)
     video_jobs[job_id] = {
         'status': 'finished',
-        'data': tracked_objects,
+        'data': detections,
     }
+
+    if DEBUG: print(video_jobs[job_id]['data']) 
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -3,37 +3,46 @@ import time
 
 sample_videos = [
     "https://storage.googleapis.com/sieve-public-videos/celebrity-videos/dwyane_basketball.mp4",
-    "https://storage.googleapis.com/sieve-public-videos/celebrity-videos/obama_interview.mp4",
-    "https://storage.googleapis.com/sieve-public-videos/celebrity-videos/elon_podcast.mp4"
 ]
 
 job_ids = []
 
 def test_push():
-    for url in sample_videos:
+    for url in sample_videos: 
         response = requests.post('http://localhost:5000/push', data={'source_name': 'sample_video', 'source_url': url})
-        job_id = response.json().get('id')
+        print("Server response:", response.json())  # Add this line to print the server's response
+        job_id = response.json().get('id (/push)')
         if job_id:
             job_ids.append(job_id)
         print(response.json())
 
-def test_status(id):
+def test_status(id): # returns 'queued', 'processing', or 'finished'
     response = requests.get(f'http://localhost:5000/status/{id}')
-    print(response.json())
+    print(f"(Job {id}):", response.json())
 
-def test_query(id):
+def test_query(id): # returns the output of the video processing job
     response = requests.get(f'http://localhost:5000/query/{id}')
     print(response.json())
 
-def test_list():
+def test_list(): # returns a list of job IDs
     response = requests.get('http://localhost:5000/list')
     print(response.json())
-
+    
 if __name__ == '__main__':
     test_push()
     test_list()
-    # Wait for the video processing to finish before testing the /status and /query endpoints
-    time.sleep(60)  # Wait for 60 seconds (adjust the waiting time based on your processing time)
-    for job_id in job_ids:
-        test_status(job_id)
-        test_query(job_id)
+
+    unfinished_jobs = set(job_ids)
+
+    # Continuously check the /status endpoint until all video processing jobs are finished
+    while unfinished_jobs:
+        for job_id in list(unfinished_jobs):
+            test_status(job_id)
+            response = requests.get(f'http://localhost:5000/status/{job_id}')
+            status = response.json().get('status')
+
+            if status == 'finished':
+                test_query(job_id)
+                unfinished_jobs.remove(job_id)
+
+        time.sleep(10)  # Wait for 10 seconds before checking the status again
